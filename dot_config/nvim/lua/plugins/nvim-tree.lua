bc54884d43977920681dd666e164f5242ea85ac6
koltrast <koltrast@pm.me>
2022-01-01 22:38:40 +0100

    remove the useless

--- dot_config/nvim/lua/plugins/nvim-tree.lua
+++ dot_config/nvim/lua/plugins/nvim-tree.lua
@@ -1,3 +1,6 @@
+local map = vim.api.nvim_set_keymap
+local default_opts = { noremap = true, silent = true }
+
 -- must be set before calling require
 vim.g.nvim_tree_indent_markers = 1 -- this option shows indent markers when folders are open
 
@@ -8,13 +11,5 @@ require('nvim-tree').setup {
     }
 }
 
-vim.api.nvim_set_keymap('n', '<Leader>n', ':NvimTreeToggle<CR>', {noremap = true, silent = true})
+map('n', '<Leader>n', ':NvimTreeToggle<CR>', default_opts)
 
--- This highlight override is based on the tokyonight
--- theme. Changing the theme will cause NvimTreeEndOfBuffer to break
-vim.cmd[[
-    augroup NvimTreeHighlightGroups
-        autocmd!
-        autocmd BufEnter,ColorScheme * highlight NvimTreeEndOfBuffer guifg=#1f2335
-    augroup END
-]]
