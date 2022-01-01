1a2d2e83ebccf815917540d5bf154605a3e4d4e4
koltrast <koltrast@pm.me>
2022-01-01 19:10:31 +0100

    solve visual issue

--- dot_config/nvim/lua/keymaps.lua
+++ dot_config/nvim/lua/keymaps.lua
@@ -19,30 +19,30 @@ map('n', 'w', '<C-w>', default_opts)
 map('n', 'W', '<C-w><C-w>', default_opts)
 
 -- [HJKL] > {CTSR}
-map('n', 'c', 'h', default_opts)    -- left
-map('n', 'r', 'l', default_opts)    -- right
-map('n', 't', 'j', default_opts)    -- up
-map('n', 's', 'k', default_opts)    -- down
-map('n', 'C', 'H', default_opts)    -- up of screen
-map('n', 'R', 'L', default_opts)    -- bottom of screen
-map('n', 'T', 'J', default_opts)    -- join
-map('n', 'S', 'K', default_opts)    -- help
+map('', 'c', 'h', default_opts)    -- left
+map('', 'r', 'l', default_opts)    -- right
+map('', 't', 'j', default_opts)    -- up
+map('', 's', 'k', default_opts)    -- down
+map('', 'C', 'H', default_opts)    -- up of screen
+map('', 'R', 'L', default_opts)    -- bottom of screen
+map('', 'T', 'J', default_opts)    -- join
+map('', 'S', 'K', default_opts)    -- help
 map('n', 'zs', 'zj', default_opts)  -- next fold
 map('n', 'zt', 'zk', default_opts)  -- prev fold
 
 -- {HJKL} < [CTSR]
 -- {J} = up to
-map('n', 'j', 't', default_opts)    -- next
-map('n', 'J', 'T', default_opts)    -- prev
+map('', 'j', 't', default_opts)    -- next
+map('', 'J', 'T', default_opts)    -- prev
 -- {L} = change
-map('n', 'l', 'c', default_opts)    -- wait a movment
-map('n', 'L', 'C', default_opts)    -- to the end of line
+map('', 'l', 'c', default_opts)    -- wait a movment
+map('', 'L', 'C', default_opts)    -- to the end of line
 -- {H} = replace
-map('n', 'h', 'r', default_opts)    -- a character
-map('n', 'H', 'R', default_opts)    -- replace mode
+map('', 'h', 'r', default_opts)    -- a character
+map('', 'H', 'R', default_opts)    -- replace mode
 -- {K} = substitute
-map('n', 'k', 'S', default_opts)    -- character
-map('n', 'K', 'S', default_opts)    -- line
+map('', 'k', 'S', default_opts)    -- character
+map('', 'K', 'S', default_opts)    -- line
 -- spell
 map('n', ']k', ']s', default_opts)
 map('n', '[k', '[s', default_opts)
