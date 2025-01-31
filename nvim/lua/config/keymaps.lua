22b880efd507aa8e103b4abf09527478fedad9d5
koltrast <koltrast@pm.me>
2025-01-31 15:01:43 +0100

    add Ergo-L keymaps tweaks to nvim

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,7 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+-- Ergo-L tweaks
+
+vim.keymap.set("n", "+", "gj", { noremap = true })
+vim.keymap.set("n", "-", "gk", { noremap = true })
+
+vim.keymap.set("n", ",", ";", { noremap = true })
+vim.keymap.set("n", ";", ",", { noremap = true })
