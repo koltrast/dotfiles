87335091e8bf865b39045ae1b3b27bca241ea3db
koltrast <koltrast@pm.me>
2025-06-01 14:42:28 +0200

    add markdown.lua config file

---
+++ nvim/lua/plugins/markdown.lua
@@ -0,0 +1,39 @@
+return {
+  {
+    -- obsidian
+    "epwalsh/obsidian.nvim",
+    version = "*",
+    lazy = true,
+    ft = "markdown",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    opts = {
+      workspaces = {
+        {
+          name = "Notes",
+          path = "~/Library/Mobile Documents/iCloud~md~obsidian/Documents/Zettlekasten/",
+        },
+      },
+      -- ui = {enable = false},
+    },
+  },
+
+  --   {
+  --     -- pretty markdown
+  --     "MeanderingProgrammer/render-markdown.nvim",
+  --     enabled = false,
+  --     opts = {},
+  --     dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" },
+  --   },
+  --
+  --   {
+  --     "iamcco/markdown-preview.nvim",
+  --     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  --     build = "npm install",
+  --     init = function()
+  --       vim.g.mkdp_filetypes = { "markdown" }
+  --     end,
+  --     ft = { "markdown" },
+  --   },
+}
