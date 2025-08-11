7999f01948b6c71b27db01368e3aeeb1d6e268d1
koltrast <koltrast@pm.me>
2025-08-11 11:55:18 +0200

    comment obsidian plugin

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -1,23 +1,23 @@
 return {
-  {
-    -- obsidian
-    "epwalsh/obsidian.nvim",
-    version = "*",
-    lazy = true,
-    ft = "markdown",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-    opts = {
-      workspaces = {
-        {
-          name = "Notes",
-          path = "~/Library/Mobile Documents/iCloud~md~obsidian/Documents/Zettelkasten/",
-        },
-      },
-      -- ui = {enable = false},
-    },
-  },
+  --   {
+  --     -- obsidian
+  --     "epwalsh/obsidian.nvim",
+  --     version = "*",
+  --     lazy = true,
+  --     ft = "markdown",
+  --     dependencies = {
+  --       "nvim-lua/plenary.nvim",
+  --     },
+  --     opts = {
+  --       workspaces = {
+  --         {
+  --           name = "Notes",
+  --           path = "~/Library/Mobile Documents/iCloud~md~obsidian/Documents/Zettelkasten/",
+  --         },
+  --       },
+  --       -- ui = {enable = false},
+  --     },
+  --   },
 
   --   {
   --     -- pretty markdown
