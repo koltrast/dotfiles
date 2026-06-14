8e3cc99ab556f174965b121245870b6b6554623c
koltrast <koltrast@pm.me>
2026-06-14 21:15:24 +0200

    change theme to rosé pine

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,17 @@
 return {
   {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    opts = {
+      variant = "auto", -- follow vim.o.background
+      dark_variant = "main", -- use "main" when background=dark
+    },
+  },
+
+  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin-mocha",
+      colorscheme = "rose-pine",
     },
   },
 }
